54e735befb17da63b27931808a9a5be771a003c7
Boanerges Junior <boanerges_junior@hotmail.com>
2021-03-24 19:57:59 -0300

    Componentes pai e filho

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
 //import BomDia from './componentes/BomDia'
 //import {BoaTarde, BoaNoite} from './componentes/Multiplos'
-import Saudacao from './componentes/Saudacao'
+//import Saudacao from './componentes/Saudacao'
+
+import Pai from './componentes/Pai'
 
 ReactDOM.render(
     <React.Fragment>
-        <Saudacao tipo="Bom dia" nome="João"/>
+        <Pai nome="Paulo" sobrenome="Ferreira"/>
     </React.Fragment>,
 document.getElementById('root'))
