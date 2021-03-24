e67864ad472d6d1232ab7120a2e5826999f8ed20
Boanerges Junior <boanerges_junior@hotmail.com>
2021-03-24 16:33:43 -0300

    Exportando multiplos componentes em um arquivos

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+//import BomDia from './componentes/BomDia'
+import {BoaTarde, BoaNoite} from './componentes/Multiplos'
 //JSX
 
-ReactDOM.render(<BomDia nome = "Boanerges" idade="23"/>, document.getElementById('root'))
+ReactDOM.render(
+    <React.Fragment>
+        <BoaTarde nome = "Boanerges" idade="23"/>
+        <BoaNoite nome = "João" idade="30"/>
+    </React.Fragment>,
+    document.getElementById('root'))
