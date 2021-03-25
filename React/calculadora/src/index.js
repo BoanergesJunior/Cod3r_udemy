5a82ed55edf14462dae4ec1b228fe8f1368328f9
Boanerges Junior <boanerges_junior@hotmail.com>
2021-03-25 14:59:11 -0300

    Criando componente Button

--- React/calculadora/src/index.js
+++ React/calculadora/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './main/Calculator'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <div>
     <h1>Calculadora</h1>
-    <Calculator />
+    <Calculator/>
   </div>,
-   document.getElementById('root')
+  document.getElementById('root')
 );
-
+registerServiceWorker()
